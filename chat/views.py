c56c43019e3ba49015a4ad9f8971518bb9c63b9f
thedezigner <angelopoku2@gmail.com>
2025-01-16 14:05:32 +0100

    Updates

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from chat.models import Room, Message
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
         return render(request, 'home.html')
 
 def room(request, room):
-        return render(request, 'room.html')
+        username = request.GET.get('username')
+        room_details = Room.objects.get(name = room)
+        return render(request, 'room.html', {
+              'username': username,
+              'room': room,
+              'room_details': room_details
+        })
 
 def checkview(request):
         room = request.POST['room_name']
@@ -15,9 +22,33 @@ def checkview(request):
         if Room.objects.filter(name = room).exists():
                 return redirect ('/' + room + '/?username' + username)
         else:
-                new_room = Room.object.create(name = room)
+                new_room = Room.objects.create(name = room)
                 new_room.save()
                 return redirect ('/' + room + '/?username' + username)
 
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    return HttpResponse('Message sent successfully')
+
+"""
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
+"""
+
+def getMessages(request, room):
+    try:
+        room_details = Room.objects.get(name=room.lower()) 
+        messages = Message.objects.filter(room=room_details.id)
+        return JsonResponse({"messages": list(messages.values())})
+    except Room.DoesNotExist:
+        return JsonResponse({"error": "Room not found"}, status=404)
 
 
